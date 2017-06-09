359962562315b91034c519efa094044024bd65a1
Parker Johnson <mrstormthebeach@gmail.com>
2017-06-09 10:26:43 -0700

    Implement WarGame

---
+++ src/main/java/prkr/war/WarGame.java
@@ -0,0 +1,51 @@
+package prkr.war;
+
+import java.util.ArrayList;
+
+import exceptions.DuplicatePlayerException;
+import exceptions.TooManyPlayersException;
+
+public class WarGame {
+	private ArrayList<Player> players = new ArrayList<Player>();
+	
+	/**
+	 * Adds a player to the game. Does not accept more than 52 players 
+	 * and will not accept two players with the same name as defined by String equality
+	 * 
+	 * @param name
+	 * @throws DuplicatePlayerException
+	 * @throws TooManyPlayersException
+	 */
+    public void addPlayer(String name) throws DuplicatePlayerException, TooManyPlayersException {
+    	if(players.contains(new Player(name))) {
+    		throw new DuplicatePlayerException();
+    	}
+    	if(players.size() >= 52) {
+    		throw new TooManyPlayersException();
+    	}
+    	
+    	players.add(new Player(name));
+    }
+    
+    /**
+     * For every player involved in the game, distributes cards equally to their respective decks.
+     * Cards are removed from the supplied deck as they are distribued. Any cards remaining
+     * after equal distribution are not removed from the deck and need to be handled separately.
+     * 
+     * @param deck
+     */
+    public void distributeCards(Deck deck) {
+    	int cardsPerPlayer = deck.getCards().size() / getPlayers().size();
+    	
+    	for(int i = 1; i <= cardsPerPlayer; i++) {
+    		for (Player player : getPlayers()) {
+        		player.getDeck().addFirst(deck.getCards().pop());
+        	}
+    	}
+    }
+    
+    public ArrayList<Player> getPlayers() {
+    	return this.players;
+    }
+
+}
---
+++ src/test/java/prkr/war/WarGameTest.java
@@ -0,0 +1,120 @@
+package prkr.war;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import exceptions.DuplicatePlayerException;
+import exceptions.TooManyPlayersException;
+
+public class WarGameTest {
+
+	WarGame warGame;
+	Deck deck;
+	
+	@Before
+	public void setUpBefore() {
+		warGame = new WarGame();
+		
+		deck = new Deck();
+		deck.shuffle();
+	}
+	
+	@Test
+	public void addPlayer_happypath_test() {
+		try {
+			warGame.addPlayer("One");
+			warGame.addPlayer("Two");
+			warGame.addPlayer("Three");
+		} catch (DuplicatePlayerException e) {
+			fail();
+		} catch (TooManyPlayersException e) {
+			fail();
+		}
+		
+		ArrayList<Player> players = warGame.getPlayers();
+		
+		assert(players.size() == 3);
+		assert(players.contains(new Player("One")));
+		assert(players.contains(new Player("Two")));
+		assert(players.contains(new Player("Three")));
+	}
+	
+	@Test(expected = DuplicatePlayerException.class)
+	public void addPlayer_force_DuplicatePlayerException_test() throws DuplicatePlayerException, TooManyPlayersException {
+		warGame.addPlayer("One");
+		warGame.addPlayer("One");
+	}
+	
+	@Test(expected = TooManyPlayersException.class)
+	public void addPlayer_force_TooManyPlayersException_test() throws DuplicatePlayerException, TooManyPlayersException {
+		setUpWithNPlayers(53);
+	}
+	
+	@Test
+	public void disributeCards_test() {
+		setUpWithNPlayersExceptionSafe(3);
+		
+		warGame.distributeCards(deck);
+		
+		ArrayList<Player> players = warGame.getPlayers();
+		
+		assert(players.get(0).getDeck().size() == 17);
+		assert(players.get(1).getDeck().size() == 17);
+		assert(players.get(2).getDeck().size() == 17);
+	}
+	
+	@Test
+	public void distributeCards_no_player_has_unequal_cards_test() {
+		// Ensure no player has the extra card and that it remains in the intiial deck
+		setUpWithNPlayersExceptionSafe(51);
+		
+		warGame.distributeCards(deck);
+
+		int globalDeckSize = warGame.getPlayers().get(0).getDeck().size();
+		for(Player player : warGame.getPlayers()) {
+			int playerDeckSize = player.getDeck().size();
+			assertEquals(playerDeckSize, globalDeckSize);
+			assertEquals(playerDeckSize, 1);
+		}
+		
+		assertEquals(deck.getCards().size(), 1);
+	}
+
+	@Test
+	public void distributeCards_52_players_test() {
+		setUpWithNPlayersExceptionSafe(52);
+		
+		warGame.distributeCards(deck);
+		
+		ArrayList<Player> players = warGame.getPlayers();
+		
+		assert(players.get(0).getDeck().size() == 1);
+	}
+	
+	private void setUpWithNPlayers(int players) throws TooManyPlayersException {
+		for (int i = 0; i < players; i++) {
+			try {
+				warGame.addPlayer("Player "+String.valueOf(i));
+			} catch (DuplicatePlayerException e) {
+				fail();
+			}
+		}
+	}
+	
+	private void setUpWithNPlayersExceptionSafe(int players) {
+		for (int i = 0; i < players; i++) {
+			try {
+				warGame.addPlayer("Player "+String.valueOf(i));
+			} catch (DuplicatePlayerException e) {
+				fail();
+			} catch (TooManyPlayersException e) {
+				fail();
+			}
+		}
+	}
+
+}
